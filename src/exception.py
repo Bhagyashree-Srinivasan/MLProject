cf490d819267fc4405da3018e72b67e5d4d6ed55
BHAGYASHREE SRINIVASAN <bhagyashree6599@gmail.com>
2024-10-04 14:30:48 +0100

    commit data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,10 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
     filename = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{3}]".format(
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         filename,
         exc_tb.tb_lineno,
         str(error)
